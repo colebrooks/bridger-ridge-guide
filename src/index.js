adb8ee8b4579ef311100c4dcc4626171931a5abc
Cole Brooks <colespencerbrooks@gmail.com>
2024-12-21 21:49:44 -0700

    Use Cesium widget instead of viewer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
+import { Cartesian3, Ion, Math as CesiumMath, Terrain, CesiumWidget } from 'cesium';
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 // Your access token can be found at: https://ion.cesium.com/tokens.
@@ -6,13 +6,13 @@ import "cesium/Build/Cesium/Widgets/widgets.css";
 
 Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZWE0NTgwYi1hYmZmLTQ4ODktYTczOS04MjQ2YjdiOWI2MjYiLCJpZCI6MjYzOTgwLCJpYXQiOjE3MzQ3NTEzNzV9.2Ad8gtF534bVAY_eatXttkDl-BI1L4K9D-1JNtJxOIc';
 
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Viewer('cesiumContainer', {
+// Initialize the Cesium Widget in the HTML element with the `cesiumContainer` ID.
+const map = new CesiumWidget('cesiumContainer', {
   terrain: Terrain.fromWorldTerrain(),
 });    
 
 // Fly the camera to Bridger Bowl at the given longitude, latitude, and height.
-viewer.camera.flyTo({
+map.camera.flyTo({
   destination: Cartesian3.fromDegrees(-110.887222, 45.815556, 3500),
   orientation: {
     heading: CesiumMath.toRadians(270.0),
